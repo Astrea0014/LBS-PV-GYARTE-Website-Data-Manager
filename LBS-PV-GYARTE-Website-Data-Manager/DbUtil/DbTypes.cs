6d479fc9578613aa841827d371e4b8651d96529a
Douglas Lyman <douglas.lyman@icloud.com>
2025-02-01 00:18:30 +0100

    Add project files.

---
+++ LBS-PV-GYARTE-Website-Data-Manager/DbUtil/DbTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager.DbUtil
+{
+    // 1:1 C# representation of the interfaces defined in 'DbTypes.ts'.
+
+    internal struct Collaboration
+    {
+        public int collaboration_id;
+        public int year;
+    }
+}
